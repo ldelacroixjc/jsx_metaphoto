1b3c7c2b6cfb1eb1dc042f384839ef333bf632f2
Juan Carlos de la Cruz <ldelacroixjc@gmail.com>
2024-09-12 09:35:25 -0600

    IFRAME 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -8,18 +6,8 @@ import { store, persistor } from './store/store';
 
 function App() {
 
-    
-    const publicUrl =
-    'http://analytics-metabase.apps.dev.relishiq.com/embed/dashboard/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZXNvdXJjZSI6eyJkYXNoYm9hcmQiOjF9LCJwYXJhbXMiOnt9LCJleHAiOjE3MjYxNTM5NTYsImlhdCI6MTcyNjE1MzM1NX0.TZv20ClLgoMDdIpxwqPsbH-bl0l9sZLSb_rt_N86EFs#bordered=true&titled=true';
-
-    return (
-        <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
-                
-                <iframe src="http://analytics-metabase.apps.dev.relishiq.com/embed/dashboard/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZXNvdXJjZSI6eyJkYXNoYm9hcmQiOjF9LCJwYXJhbXMiOnt9LCJleHAiOjE3MjYxNTQ3NzEsImlhdCI6MTcyNjE1NDE3MX0.BZwIbL1pwzS6wpJSJ-QN_ZR9xwKpBJwglZTp-sHKOss#bordered=true&titled=true" width={1560} height={850} />
-               
-            </PersistGate>
-        </Provider>		
+    return (        
+        <iframe src="http://analytics-metabase.apps.dev.relishiq.com/embed/dashboard/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZXNvdXJjZSI6eyJkYXNoYm9hcmQiOjF9LCJwYXJhbXMiOnt9LCJleHAiOjE3MjYxNTU4OTUsImlhdCI6MTcyNjE1NTI5NH0.nfoMbSiE8Li5mytRYt0YUoGl5pS5alP6X-_6XqslvFY#bordered=true&titled=true" width={1560} height={850} />
     );
 }
 
